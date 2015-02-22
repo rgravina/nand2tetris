f8f13b9c7e4f2db397cf125e770444233d9f0c01
Robert Gravina <robert.gravina@gmail.com>
2015-02-22 13:38:23 +0900

    Implemented Multi.asm
    Also added in Sum.asm to get familiar with Hack ASM.

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -1,9 +1,34 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// You can mutiply a number X by Y using addition by adding X+X+X...
+// Y times.
+//
 
-// Put your code here.
+  // counter to keep track of additions
+  @i
+  M=0
+  // Initialise R2
+  @R2
+  M=0
+(LOOP)
+  // if (i=R1) goto END
+  @i
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JEQ
+  // Add R0 to R2
+  @R0
+  D=M
+  @R2
+  M=D+M
+  // i++
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
