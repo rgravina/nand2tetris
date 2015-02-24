d99f9032b81c12c1a2679ef71185b95c5cf0cefe
Robert Gravina <robert.gravina@kii.com>
2015-02-24 18:13:26 +0900

    Added argument to makefile.

---
+++ Hack/Parser.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  Hack
+//
+//  Created by Robert Gravina on 2/24/15.
+//  Copyright (c) 2015 Robert Gravina. All rights reserved.
+//
+
+import Foundation
--- Hack/main.swift
+++ Hack/main.swift
@@ -1,7 +1,12 @@
 import Foundation
 
-println("Usage: hack <input.asm>")
-println()
-println("An assembler for the Hack platform.")
+func usage() {
+  println("Usage: hack <input.asm>")
+  println()
+  println("An assembler for the Hack platform.")
+}
 
+if Process.arguments.count != 2 {
+  usage()
+}
 
