a991254fcc82d80aee81ab5b14ccbd82e58dc055
Robert Gravina <robert.gravina@gmail.com>
2015-02-26 21:53:55 +0900

    Strip whitespace.

--- HackC/parser.c
+++ HackC/parser.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
+#include <ctype.h>
+
+int get_next_char(FILE *file, int current_file_pointer);
+int skip_whitespace(FILE *file , int current_file_pointer);
 
 void parse(char* filename) {
   FILE *file = fopen(filename, "r");
   if (file == 0) {
     printf("Could not open file\n");
   } else {
-    int x;
-    while  ((x = fgetc(file)) != EOF) {
-      printf("%c", x);
+    int current_file_pointer;
+    while ((current_file_pointer = get_next_char(file, current_file_pointer)) != EOF) {
+      printf("%c", current_file_pointer);
     }
     fclose(file);
   }
+}
+
+int get_next_char(FILE *file , int current_file_pointer) {
+  current_file_pointer = fgetc(file);
+  current_file_pointer = skip_whitespace(file, current_file_pointer);
+  return current_file_pointer;
+}
+
+int skip_whitespace(FILE *file , int current_file_pointer) {
+  while (current_file_pointer != '\n' && isspace(current_file_pointer) ) {
+    current_file_pointer = fgetc(file);
+  }
+  return current_file_pointer;
 }
