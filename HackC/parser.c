f2c1240092c5a4aca7893b2e3889d04ccd095f24
Robert Gravina <robert.gravina@gmail.com>
2015-02-28 18:43:27 +0900

    For now, just print every character skipped.

--- HackC/parser.c
+++ HackC/parser.c
@@ -1,31 +1,77 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
 
-int get_next_char(FILE *file, int current_file_pointer);
-int skip_whitespace(FILE *file , int current_file_pointer);
+int get_next_char(FILE *file, int c);
+int skip_to_next_command(FILE *file , int c);
+int skip_whitespace(FILE *file , int c);
+int skip_comments(FILE *file , int c);
 
 void parse(char* filename) {
   FILE *file = fopen(filename, "r");
   if (file == 0) {
     printf("Could not open file\n");
   } else {
-    int current_file_pointer;
-    while ((current_file_pointer = get_next_char(file, current_file_pointer)) != EOF) {
-      printf("%c", current_file_pointer);
-    }
+    // int current_file_pointer;
+    // bool in_token = false;
+    // while ((current_file_pointer = get_next_char(file, current_file_pointer)) != EOF) {
+    //   if (current_file_pointer != '\n') {
+    //     printf("%c", current_file_pointer);
+    //     in_token = false;
+    //   } else {
+    //     in_token = true;
+    //   }
+    // }
+
+    // current char
+    int c = 0;
+
+    // read first char
+    c = fgetc(file);
+    printf("%c", c);
+    skip_to_next_command(file, c);
+
+    printf("Next char: %i, %c", c, c);
     fclose(file);
   }
 }
 
-int get_next_char(FILE *file , int current_file_pointer) {
-  current_file_pointer = fgetc(file);
-  current_file_pointer = skip_whitespace(file, current_file_pointer);
-  return current_file_pointer;
+// int get_next_char(FILE *file , int c) {
+//   c = fgetc(file);
+//   c = skip_whitespace(file, c, line_number);
+//   c = skip_comments(file, c, line_number);
+//   return c;
+// }
+
+
+int skip_to_next_command(FILE *file , int c) {
+  while (c != EOF) {
+    c = skip_whitespace(file, c);
+    c = skip_comments(file, c);    
+  }
+  return c;
 }
 
-int skip_whitespace(FILE *file , int current_file_pointer) {
-  while (current_file_pointer != '\n' && isspace(current_file_pointer) ) {
-    current_file_pointer = fgetc(file);
+int skip_whitespace(FILE *file , int c) {
+  while (isspace(c)) {
+    c = fgetc(file);
+    printf("%c", c);
   }
-  return current_file_pointer;
+  return c;
+}
+
+// will skip anything starting with a forward slash to the end of the line.
+// Although comments start with two forward slashes, slashes don't appear
+// in assembler instructions so we can avoid looking ahead on character. 
+int skip_comments(FILE *file , int c) {
+  if (c == '/') {
+    while (c != '\n') {
+      c = fgetc(file);
+      printf("%c", c);
+    }
+    //consume end of line
+    c = fgetc(file);
+    printf("%c", c);
+  }
+  return c;
 }
