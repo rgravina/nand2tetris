858002a69666d8648013e958bd2d4e99d0f23f08
Robert Gravina <robert.gravina@kii.com>
2015-03-27 15:23:10 +0900

    Fix error in usage.

--- JackC/jack.c
+++ JackC/jack.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
 void usage(char* executable_name) {
   printf("usage: %s <input>\n", executable_name);
   printf("A Jack compiler for the Hack platform.\n\n");
-  printf("<input.asm>    - Outputs machine code from assembly language\n");
-  printf("<input.vm>     - Outputs VM code from assembly language\n");
-  printf("<directory>    - Outputs VM code from directory of assembly language files\n");
+  printf("<input.asm>    - Outputs machine code from assembly code\n");
+  printf("<input.vm>     - Outputs assembly code from VM code\n");
+  printf("<directory>    - Outputs assembly code from directory of VM code files\n");
 }
