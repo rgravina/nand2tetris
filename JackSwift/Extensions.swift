4a912ac979828f34034baf8bdbb6540fb1fd093a
Robert Gravina <robert.gravina@gmail.com>
2015-03-29 20:23:44 +0900

    Added comments/formatting to string extension.

--- JackSwift/Extensions.swift
+++ JackSwift/Extensions.swift
@@ -1,21 +1,21 @@
 import Foundation
 
 //
-// String extension from http://oleb.net/blog/2014/07/swift-strings/
+// String subscript extension from http://oleb.net/blog/2014/07/swift-strings/
 //
 extension String
 {
-  subscript(integerIndex: Int) -> Character
-    {
-      let index = advance(startIndex, integerIndex)
-      return self[index]
+  // e.g. string[0]
+  subscript(integerIndex: Int) -> Character {
+    let index = advance(startIndex, integerIndex)
+    return self[index]
   }
 
-  subscript(integerRange: Range<Int>) -> String
-    {
-      let start = advance(startIndex, integerRange.startIndex)
-      let end = advance(startIndex, integerRange.endIndex)
-      let range = start..<end
-      return self[range]
+  // e.g. string[0..<2]
+  subscript(integerRange: Range<Int>) -> String {
+    let start = advance(startIndex, integerRange.startIndex)
+    let end = advance(startIndex, integerRange.endIndex)
+    let range = start..<end
+    return self[range]
   }
 }
