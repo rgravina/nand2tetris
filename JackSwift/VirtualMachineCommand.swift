03ce000bd1e28ef942910ebda94a3a368bd8799a
Robert Gravina <robert.gravina@gmail.com>
2015-04-17 14:11:23 +0900

    Start of VM parser.

--- JackSwift/VirtualMachineCommand.swift
+++ JackSwift/VirtualMachineCommand.swift
@@ -1,10 +1,24 @@
 import Foundation
 
 enum VirtualMachineCommandType {
+  case Arithmetic, Push, Pop, Label, Goto, If, Function, Return, Call, Unknown
 }
 
 class VirtualMachineCommand {
+  let type:VirtualMachineCommandType
+
   init(command: String) {
-    println("\(__FILE__):\(__LINE__): \(command)")
+    let tokens = split(command) {$0 == " "}
+    switch(tokens.first!) {
+    case "push":
+      type = .Push
+    case "push":
+      type = .Pop
+    case "add":
+      type = .Arithmetic
+    default:
+      type = .Unknown
+    }
+    println(tokens)
   }
 }
