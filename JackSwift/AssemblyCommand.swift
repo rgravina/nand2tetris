e973747b329a6b8f8a755194ff13706f73888dcd
Robert Gravina <robert.gravina@gmail.com>
2015-04-21 00:04:34 +0900

    Parse the computation instruction into parts.

--- JackSwift/AssemblyCommand.swift
+++ JackSwift/AssemblyCommand.swift
@@ -23,8 +23,32 @@ public class AssemblyCommand : Printable {
       address = command[1..<count(command)]
     case "(":
       type = .Label
+      address = command[1..<count(command)-1]
     default:
       type = .Computation
+      /**
+       * Now we can work out the C instruction fields
+       *
+       * C instruction
+       * dest=comp;jump
+       * either dest or jump may be empty
+       * if dest is empty. the '=' is omitted
+       * if jump is empty, the ';' is omitted
+       *
+       * i.e dest=comp;jump, comp;jump, dest=comp.
+       */
+      let equalsIndex = command.rangeOfString("=")
+      let semiColonIndex = command.rangeOfString(";")
+      if (equalsIndex != nil) {
+        dest = command[command.startIndex..<equalsIndex!.startIndex]
+      } else {
+        comp = command[command.startIndex..<semiColonIndex!.startIndex]
+      }
+      if (semiColonIndex != nil) {
+        jump = command[semiColonIndex!.endIndex..<command.endIndex]
+      } else {
+        comp = command[equalsIndex!.endIndex..<command.endIndex]
+      }
     }
     println(self)
   }
@@ -38,9 +62,17 @@ public class AssemblyCommand : Printable {
       case .Address:
         return "@\(address!)"
       case .Computation:
-        return "todo"
+        var instruction = ""
+        if (dest != nil) {
+          instruction += "\(dest!)="
+        }
+        instruction += comp!
+        if (jump != nil) {
+          instruction += ";\(jump!)"
+        }
+        return instruction
       case .Label:
-        return "todo"
+        return "(\(address!))"
       default:
         return "Unknown"
       }
