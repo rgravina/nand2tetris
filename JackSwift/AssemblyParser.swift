6e2e60a8f8ddef584c020d73d68fd99f41a36db1
Robert Gravina <robert.gravina@gmail.com>
2015-04-21 14:12:12 +0900

    Add comments for first and second pass of the assembler.

--- JackSwift/AssemblyParser.swift
+++ JackSwift/AssemblyParser.swift
@@ -3,20 +3,38 @@ import Foundation
 class AssemblyParser {
   let reader:HackFileReader
   let symbolTable:AssemblerSymbolTable
+  var commands = [AssemblyCommand]()
+  var currentCommand = 0
   /**
-  * Open the file
+  * Open the file, reads in input and parses all commands
   */
   init(file: String) {
     reader = HackFileReader(file: file)
     symbolTable = AssemblerSymbolTable()
+    /**
+     * First pass - read instructions and parse A and C instructions.
+     * If an A instruction refers to an undefined symbol or label, store
+     * the symbol name in the address field. If a label is found, add it to
+     * the symbol table.
+     */
+    while let line = reader.nextCommand() {
+      commands.append(AssemblyCommand(command: line))
+    }
+
+    /**
+     * Second pass - resolve previously undefined symbols.
+     * Symbols already in the symbol table refer to labels in the assembly code.
+     * These can be simply replaced. For others, they must be variables so
+     * they should be allocated in RAM.
+     */
   }
 
   /**
-  * Returns the next command and advances the current line (skips whitespace and blank lines)
+  * Returns the next command
   */
   func advance() -> AssemblyCommand? {
-    if let line = reader.nextCommand() {
-      return AssemblyCommand(command: line)
+    if currentCommand < count(commands) {
+      return commands[currentCommand++]
     }
     return nil
   }
