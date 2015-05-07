ab730b9eef166d863fe582280513f9decb89a1a5
Robert Gravina <robert.gravina@kii.com>
2015-05-07 17:01:14 +0900

    Spec fix.

--- JackTest/VirtualMachineTest.swift
+++ JackTest/VirtualMachineTest.swift
@@ -7,8 +7,8 @@ class VirtualMachineTest: QuickSpec {
   override func spec() {
     describe("a VM command") {
       it("should parse a valid push command") {
-        let vmc = VirtualMachineCommand(command: "push constant 1")
-        expect(vmc.description).to(equal("push constant 1"))
+        let vmc = VirtualMachineCommand(className: "Class1", command: "push constant 1")
+        expect(vmc.description).to(equal("// push constant 1"))
         expect(vmc.type).to(equal(VirtualMachineCommandType.Push))
         expect(vmc.arg1).to(equal("constant"))
         expect(vmc.arg2).to(equal(1))
